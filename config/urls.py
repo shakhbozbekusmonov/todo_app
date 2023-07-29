5bcc2ac97d8ca4afb4e1e0f939fa7619910eea2b
Shakhbozbek Usmonov <83509440+shakhbozbekusmonov@users.noreply.github.com>
2023-07-29 07:56:11 +0500

    Initial commit

---
+++ config/urls.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.permissions import AllowAny
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="News API",
+        default_version="v1",
+        description="News API CRUD operations",
+        terms_of_service="https://miracleprogrammer.com/",
+        contact=openapi.Contact(email="shakhbozbek.usmonov@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('api/v1/', include('apps.urls')),
+    path('api-auth', include('rest_framework.urls')),
+    path('api/v1/auth/', include('dj_rest_auth.urls')),
+    path('api/v1/auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
